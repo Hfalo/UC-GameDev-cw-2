2dba6a202bc104db55d30539931d13abbdb56782
HIND <107958492+Hfalo@users.noreply.github.com>
2022-06-25 22:37:44 +0300

    class work 2

---
+++ unicodecs2_vs/unicodecs2_vs/Program.cs
@@ -0,0 +1,37 @@
+﻿string heroName = "Eula Lawrence";
+string heroElement = "Cryo";
+
+string villianName = "Tartaglia";
+string villianElement = "Hydro";
+
+int Cryo = 100;
+int Hydro = 50;
+
+if (Cryo > Hydro)
+{
+    Console.WriteLine(heroName + " wins !");
+}
+else if (Cryo == Hydro)
+{
+    Console.WriteLine("No one wins, better luck next time");
+}
+else if (Cryo < Hydro)
+{
+    Console.WriteLine(villianName + " wins !");
+}
+
+
+// excersice 2
+
+float Playerspeed = 5.5f;
+
+Setspeed(2.5f);
+
+void Setspeed(float speed)
+{
+    Console.WriteLine("old speed : " + Playerspeed);
+    Playerspeed = speed;
+    Console.WriteLine("new speed : " + Playerspeed);
+}
+
+
